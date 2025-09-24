5b6c5c46ee15cad625b28a41d29b1ef2181a840b
dsvisilva <sdavih06@gmail.com>
2025-09-24 16:18:02 -0300

    js alterations

--- scripts.js
+++ scripts.js
@@ -1,25 +1,53 @@
 const convertButton = document.querySelector("#convert-button");
+const currencySelect = document.querySelector("#currency-select");
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector("#input-currency").value;
-  const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
+  const currencyValueToConvert = document.querySelector(
+    ".currency-value-to-convert"
+  );
   const currencyValueConverted = document.querySelector(".currency-value");
 
-  const dolarToday = 5.2;
+  console.log(currencySelect.value);
 
-  const convertedValue = inputCurrencyValue / dolarToday;
+  const dolarToday = 5.2;
+  const euroToday = 6.2;
+
+  if (currencySelect.value == "USD") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue / dolarToday);
+  }
+
+  if (currencySelect.value == "EUR") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrencyValue / euroToday);
+  }
 
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-    style: "currency",  
-    currency: "BRL"
-  }).format(inputCurrencyValue);
-
-  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "USD"
-  }).format(convertedValue);
-
-  console.log(convertedValue);
+    currency: "BRL",
+  }).format(inputCurrencyValue);
 }
 
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name");
+    const currencyImg = document.getElementById("currency-img");
+
+    if (currencySelect.value == "USD") {
+        currencyName.innerHTML = "Dólar";
+        currencyImg.src = "./assets/usa.png";
+    } 
+    if (currencySelect.value == "EUR") {
+        currencyName.innerHTML = "Euro";
+        currencyImg.src = "./assets/euro.png";
+    }   
+    
+    convertValues();
+}
+
+currencySelect.addEventListener("change", changeCurrency);
 convertButton.addEventListener("click", convertValues);
